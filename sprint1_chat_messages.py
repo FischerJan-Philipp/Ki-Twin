538cd21b7940dd7426a3cc4177faaa502867562f
ky272 <77926314+ky272@users.noreply.github.com>
2023-10-27 17:43:40 +0200

    jan chat messages

--- sprint1_chat_messages.py
+++ sprint1_chat_messages.py
@@ -14,56 +14,42 @@ from langchain.document_loaders import PyPDFLoader
 import os
 import getpass
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 
 os.environ.get("OPENAI_API_KEY")
 
-loader = PyPDFLoader('Logistik ZUSAMMENFASSUNG.pdf')
-pages = loader.load_and_split()
-
-text=[]
-for page in pages:
-    text.append(page.page_content)
-
-combinedPDF = " ".join(text)
-
-
-
-
-text_splitter = CharacterTextSplitter(
-        #100 characters per chunk
-        chunk_size=100, chunk_overlap=10
-    )
+with open('gefilterte_nachrichten_emoji.txt') as f:
+    chats = f.read()
 
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size = 100,
+    chunk_size = 30,
     chunk_overlap  = 20,
     length_function = len,
     add_start_index = True,
 )
 
-docs = text_splitter.create_documents([combinedPDF])
+docs = text_splitter.create_documents([chats])
 print(docs[2].page_content)
 
 #create embeddings
 faiss_index = FAISS.from_documents(docs, OpenAIEmbeddings())
 
 #save the index database
-faiss_index.save_local("faiss_logistikPDF_chunk_100_overlap_10")
+faiss_index.save_local("jan_chats_30_20")
 
 #load the index database
-db = FAISS.load_local("faiss_index", OpenAIEmbeddings())
+db = FAISS.load_local("jan_chats_30_20", OpenAIEmbeddings())
 
 #similarity search function
 def retrieve_info(query):
-    similar_documents = faiss_index.similarity_search(query, k=3)
+    similar_documents = faiss_index.similarity_search(query, k=12)
     page_contents_array = [doc.page_content for doc in similar_documents]
     print(page_contents_array)
     return page_contents_array
 
 #similarity search
-query = "Was sind die Aufgaben der Logsitik?"
+query = "Wann gehen wir saufen bro?"
 similar_documents = retrieve_info(query)
 similar_documents[1].page_content
 
@@ -72,25 +58,25 @@ similar_documents[1].page_content
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
 
 template = """
-    You are a student working with the following notes:
-    {notes}
-    1/ Your answers should be in the style of the notes you have read, only use the given information there.
-    2/ If there is no relevant information in the notes, say "I don't know".
+    You are Jan, a business computing student at the HTW Berlin. You are chatting with your friend on WhatsApp.
+    Your friends message is: {message}
+    
+    1/ Your answers should be in the style of your answers provided here: {chats}
     
     Please write the best response that you can.
 """
 
 prompt = PromptTemplate(
     template=template,
-    input_variables=["notes"]
+    input_variables=["message", "chats"]
 )
 
 chain = LLMChain(llm=llm, prompt=prompt)
 
 #3.Retrieval augmented generation
 def generate_response(query):
-    notes = retrieve_info(query)
-    response = chain.run(notes=notes)
+    chats = retrieve_info(query)
+    response = chain.run(chats=chats, message=query)
     return response
 
 response = generate_response(query)
