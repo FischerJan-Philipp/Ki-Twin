557fc45bfc4bb3917dedbd421459544a5ac193d4
ky272 <77926314+ky272@users.noreply.github.com>
2023-11-09 20:09:10 +0100

    Jans voice cloned

--- UI.py
+++ UI.py
@@ -1,14 +1,21 @@
-import pandas
 import streamlit as st
 from pandasai.llm.openai import OpenAI
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from pandasai import PandasAI
 import calendar_chat as cc
+from elevenlabs import voices, generate, set_api_key, generate, stream
+from elevenlabs import Voice, VoiceSettings, generate
+from pathlib import Path
+
+
+#dotenv_path = Path('D:\Kyron\Documents\Python Scripts\Ki-Twin\.env')
+#load_dotenv(dotenv_path=dotenv_path)
 
 load_dotenv()
 
+elevenlabs_api_key = os.getenv('ELEVENLABS_API_KEY')
+set_api_key(elevenlabs_api_key)
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
 chat = cc.ChatCalendar()
@@ -28,3 +35,13 @@ if input_text is not None:
         st.info("Your Query: " + input_text)
         result = chat.test(input_text)
         st.success(result)
+        audio_stream = generate(
+            text=result,
+            voice=Voice(
+                voice_id="2sFP0IlelRrLzaBV9PuJ",
+                settings=VoiceSettings(stability=0.3, similarity_boost=0.35, style=0.66, use_speaker_boost=True)
+            ),
+            stream=True,
+            model="eleven_multilingual_v2"
+        )
+        stream(audio_stream)
